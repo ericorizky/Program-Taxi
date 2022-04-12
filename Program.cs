569119c5aa4111c80ec1a52f29744705b711ac92
ericorizky <ericorizky@students.amikom.ac.id>
2022-04-12 19:07:16 +0700

    Program-Taxi

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTaxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName  = "Erico";
+            taxi.OnDuty      = "YES";
+            taxi.NumPassager = "20";
+
+            taxi.PickUpPassager();
+            taxi.DropOffPassager();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTaxi
+{
+    internal class Taxi
+    {
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public string NumPassager { get; set; }
+
+        public void PickUpPassager()
+        {
+            Console.WriteLine("Driver Name        : {0}", DriverName);
+            Console.WriteLine("On Duty            : {0}", OnDuty);
+            Console.WriteLine("Number of Passager : {0}", NumPassager);
+            Console.WriteLine("\nSedang Menjemput Penumpang");
+
+        }
+        public void DropOffPassager()
+        {
+            Console.WriteLine("Selesai Mengantar Penumpang",DriverName);
+        }
+    }
+}
